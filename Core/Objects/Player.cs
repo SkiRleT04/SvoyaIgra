b74b8f6748dbe686709cbeb42d5b308f8901e19a
Ssshivaaa <artydev0101@gmail.com>
2018-02-10 21:10:56 +0200

    Core Architecture

---
+++ Core/Objects/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Objects
+{
+    public class Player
+    {
+        public int Points { get; set; }
+        public string Login { get; set; }
+        
+    }
+}
---
+++ Core/Packets/Request/BaseRequest.cs
@@ -0,0 +1,14 @@
+﻿using Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Packets.Request
+{
+    public abstract class BaseRequest
+    {
+        public  RequestType Type { get ; }
+    }
+}
